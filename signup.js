bb8f3f63c6ff743c144a7b718688bc68d3240377
smita patil <smitakpatil26@gmail.com>
2019-11-06 14:46:02 +0530

    added validation

--- signup.js
+++ signup.js
@@ -1,3 +1,27 @@
+let Fname;
+let Lname;
+let Email;
+let password;
+let rPassword;
+
 $("#signupbtn").click(function(){
     console.log("sdjhf")
+    email=$(".email").val();
+    password=$(".password").val();
+    passwordRepeat=$(".repeat-password").val();
+
+    console.log(email);
+    console.log(password);
+    if(password!=passwordRepeat)
+    {
+        alert("password is not same !!!");
+    }
+    if(!email || !password || !passwordRepeat)
+    {
+        alert("Please fill in all fields");
+    }
+    if(password.length<6)
+    {
+        alert('Password should be at least 6 characters');
+    }
 })
