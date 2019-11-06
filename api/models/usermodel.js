470e26a74b8613d9d4824ad01976b3836b3dfe47
smita patil <smitakpatil26@gmail.com>
2019-11-06 15:44:56 +0530

    created schema

---
+++ api/models/usermodel.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose');
+
+const userSchema= new mongoose.Schema({
+
+    // _id:mongoose.Schema.Types.ObjectId,
+
+    email:{
+       type: String,
+       required:true
+    },
+    password:{
+        type:String,
+        required:true,
+        // minlength:8
+    }
+});
+
+module.exports=mongoose.model('User',userSchema);
