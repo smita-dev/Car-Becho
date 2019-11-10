3355cf2a489a8c7147d15567429d17987f13f00b
smita patil <smitakpatil26@gmail.com>
2019-11-10 22:47:39 +0530

    added ajax call

--- predict.js
+++ predict.js
@@ -2,8 +2,28 @@ window.addEventListener("load", init);
 let result;
 let carList = document.getElementById("car-datalist");
 function init()
-{    
+{
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "http://localhost:5000/car/Carprice/",
+            success: function(data){
+               result=data;
+              console.log(result);
+            //   result.forEach(v=>{
+            //     console.log(v.company);
+                result.forEach(v => {
+                    let optionCompany = document.createElement("option");
+                    optionCompany.value = v.company;
+                    carList.appendChild(optionCompany);
+                });
+            // })
+            },
+            error:function(err){
+                console.log(err);
+            }
+        });
         function getFilter(){
-            console.log(result)
+            // console.log(result)  
         }
 }
