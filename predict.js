4dcc0ac95738e24bcbafde12db3532839223dfc6
smita patil <smitakpatil26@gmail.com>
2019-11-10 22:30:47 +0530

    added predict.js file

--- predict.js
+++ predict.js
@@ -2,12 +2,8 @@ window.addEventListener("load", init);
 let result;
 let carList = document.getElementById("car-datalist");
 function init()
-{
-   
-        
+{    
         function getFilter(){
             console.log(result)
-            
-            
         }
 }
